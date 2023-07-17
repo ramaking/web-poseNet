c9d13e7f78cf21db34b64fef699994d63f7809bf
ramaking <aparter01@naver.com>
2023-07-18 01:09:40 +0900

    add canvas download function

--- posenet.js
+++ posenet.js
@@ -4,6 +4,16 @@ const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+const btn_start = document.querySelector("#btn_start");
+const btn_stop = document.querySelector("#btn_stop");
+const video_recorded = document.querySelector("#video_recorded");
+
+// MediaRecorder(녹화기) 변수 선언
+let mediaRecorder = null;
+
+// 스트림 데이터를 담아둘 배열 생성
+const arrVideoData = [];
+
 //webcam을 enable하는 코드
 navigator.mediaDevices
   .getUserMedia({ video: true, audio: false })
@@ -46,6 +56,8 @@ posenet.load().then((model) => {
         (Math.atan((y2 - cy) / (x2 - cx)) * 180) / Math.PI -
         (Math.atan((y1 - cy) / (x1 - cx)) * 180) / Math.PI;
 
+      context.fillText(Math.ceil(angle), cx + 30, cy - 30);
+
       //   console.log(angle);
 
       drawKeypoints(pose.keypoints, 0.6, context); //정확도
@@ -54,6 +66,54 @@ posenet.load().then((model) => {
     requestAnimationFrame(predict); //frame이 들어올 때마다 재귀호출
   }
 });
+
+// "녹화시작" 버튼 이벤트 처리
+btn_start.onclick = (event) => {
+  // 캔버스 영역 화면을 스트림으로 취득
+  const mediaStream = canvas.captureStream();
+
+  // MediaRecorder(녹화기) 객체 생성
+  mediaRecorder = new MediaRecorder(mediaStream);
+
+  // MediaRecorder.dataavailable 이벤트 처리
+  mediaRecorder.ondataavailable = (event) => {
+    // 스트림 데이터(Blob)가 들어올 때마다 배열에 담아둔다.
+    arrVideoData.push(event.data);
+  };
+
+  // MediaRecorder.stop 이벤트 처리
+  mediaRecorder.onstop = (event) => {
+    // 들어온 스트림 데이터들(Blob)을 통합한 Blob객체를 생성
+    const blob = new Blob(arrVideoData);
+
+    // BlobURL 생성: 통합한 스트림 데이터를 가르키는 임시 주소를 생성
+    const blobURL = window.URL.createObjectURL(blob);
+
+    // 재생 구현
+    video_recorded.src = blobURL;
+    video_recorded.play();
+
+    // 다운로드 구현
+    const $anchor = document.createElement("a"); // 앵커 태그 생성
+    document.body.appendChild($anchor);
+    $anchor.style.display = "none";
+    $anchor.href = blobURL; // 다운로드 경로 설정
+    $anchor.download = "test.webm"; // 파일명 설정
+    $anchor.click(); // 앵커 클릭
+
+    // 배열 초기화
+    arrVideoData.splice(0);
+  };
+
+  // 녹화 시작
+  mediaRecorder.start();
+};
+
+// "녹화종료" 버튼 이벤트 처리
+btn_stop.onclick = (event) => {
+  // 녹화 중단!
+  mediaRecorder.stop();
+};
 
 /* PoseNet을 쓰면서 사용하는 함수들 코드 - 그냥 복사해서 쓰기*/
 
